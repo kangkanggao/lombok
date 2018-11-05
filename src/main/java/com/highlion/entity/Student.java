8a100fd322d1f58bd1430ebc17bd2bd6f5ea3609
kang <410522424@qq.com>
2018-11-05 09:14:28 +0800

    添加了 main方法测试

--- src/main/java/com/highlion/entity/Student.java
+++ src/main/java/com/highlion/entity/Student.java
@@ -1,10 +1,29 @@
 package com.highlion.entity;
 
-import lombok.Getter;
+import lombok.Data;
 
+@Data
 public class Student {
-	@Getter
+	
   private int id;
-	@Getter
+	
   private String name;
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+  
+  
 }
---
+++ src/main/java/com/highlion/test/Test.java
@@ -0,0 +1,15 @@
+package com.highlion.test;
+
+import com.highlion.entity.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Student stu=new Student();
+        stu.setId(1);
+        stu.setName("zhangsan");
+        System.out.println(stu.getName());
+	}
+
+}
