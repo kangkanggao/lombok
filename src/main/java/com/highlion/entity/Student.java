7d68a65ec8e570c4f0afa7eb4d8434b75925b82b
gaokangkang <gkk15711255657@163.com>
2018-11-14 09:34:30 +0800

    gkk 添加 Student 类的pwd属性

--- src/main/java/com/highlion/entity/Student.java
+++ src/main/java/com/highlion/entity/Student.java
@@ -8,6 +8,16 @@ public class Student {
   private int id;
 	
   private String name;
+  
+  private String pwd;
+
+public String getPwd() {
+	return pwd;
+}
+
+public void setPwd(String pwd) {
+	this.pwd = pwd;
+}
 
 public int getId() {
 	return id;
